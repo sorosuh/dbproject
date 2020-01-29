6a65ef4b7ec47f35fb0b3c7d6bf41fa886d26839
Mohammad <esmaeely1377@yahoo.com>
2020-01-29 17:28:33 +0330

    dfgdsg

--- backend/restaurarntSignUp.php
+++ backend/restaurarntSignUp.php
@@ -3,31 +3,36 @@
     include 'addFunction.php';
 
     $pass= $_POST["pass"];
-    $passValid= FALSE;
-    if( strlen($pass)< 8);
-    //     for($i= 0; i<=)
-
-    // }
 
     if(isset($_POST["user"]) && isset($_POST["city"]) && isset($_POST["regionID"]) && isset($_POST["street"]) && 
         isset($_POST["number"]) && isset($_POST["Xcoordinates"]) && isset($_POST["Ycoordinates"]) && 
-        isset($_POST["pass"]) && isset($_POST["qid"]) && isset($_POST["answer"]) ) {
+        isset($_POST["pass"]) && isset($_POST["qid"]) && isset($_POST["answer"])) {
         
         $user= $_POST["user"];
+        if(!isUniqueUser($user)){
+            // user is Repetitious (tekrarie)
+        }
+        $pass= $_POST["pass"];
+        if(passValid($pass)){
+            // pass not valid
+        }
+
         $city= $_POST["city"];
         $regionID= $_POST["regionID"];
         $street= $_POST["street"];
         $number= $_POST["number"];
         $Xcoordinates= $_POST["Xcoordinates"];
         $Ycoordinates= $_POST["Ycoordinates"];
-        $pass= $_POST["pass"];
         $qis= $_POST["qid"];
         $answer= $_POST["answer"];
 
+        addToUsers($user, $pass, 2);
+        addToQuestionUser($user, $qid, $answer);
         
+        $sql= "INSERT INTO restaurants(user,city,regioID,street,number,Xcoordinates,Ycoordinates) VALUES (?,?,?,?,?,?,?)";
+        $create= $connect->prepare($sql);
+        $create->execute([$user, $city, $regionID, $street, $number, $Xcoordinates, $Ycoordinates]);
 
     }
-
-
 
 ?>
